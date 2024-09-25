0ea2182119a4dae1a7ca318f7816f9fbf44ec2a5
Abhinav Mishra <mishraabhinav2306@gmail.com>
2024-09-25 23:19:43 +0530

    whatsapp grp link, timeline

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,43 +4,68 @@ import image from '../../assets/exploreProgramme1.png'
 
 const About = () => {
     return (
-        <>
-            <div className='aboutsection' id="about">
-                <div className="aboutheading">&lt;what is ridehack?&gt;</div>
-                <div className="aboutcontent">
-                    <div className="para">
-                        <div className="phasediv">
-                        <div className="phase">PHASE1</div>
-                        </div>
-                    <div>All four universities will conduct the Hackathon at their respective campuses.</div> 
-                    </div>
+      <>
+        <div className="aboutsection" id="about">
+          <div className="aboutheading">&lt;what is ridehack?&gt;</div>
+          <div className="aboutcontent">
+            <div className="para">
+              <div className="phasediv">
+                <div className="phase">PHASE1</div>
+              </div>
+              <div>
+                All four universities will conduct the Hackathon at their
+                respective campuses.
+              </div>
+            </div>
 
-                    <div className="para">
-                        <div className="phasediv">
-                        <div className="phase">PHASE2</div>
-                        </div>
-                    <div>Shortlisted teams from different universities in Phase 1 will participate in Phase 2 at Sector 62 campus of JIIT Noida. Accommodation for the selected teams from JUIT, JUET, and JUA will be provided at JIIT Sec 62 hostels. Tentatively, the Phase 2 will be organised at the beginning of Academic Year 2024-25.</div> 
-                    </div>
+            <div className="para">
+              <div className="phasediv">
+                <div className="phase">PHASE2</div>
+              </div>
+              <div>
+                Shortlisted teams from different universities in Phase 1 will
+                participate in Phase 2 at Sector 62 campus of JIIT Noida.
+                Accommodation for the selected teams from JUIT, JUET, and JUA
+                will be provided at JIIT Sec 62 hostels. The Phase
+                2 will be happening on 28th and 29th september 2024.
+              </div>
+            </div>
 
-                    <div className="para">
-                        <div className="phasediv">
-                        <div className="phase">PHASE3</div>
-                        </div>
-                    <div>Selected 50 teams from Phase 2 will participate in Phase 3. It is a 2 months long phase where shortlisted teams will develop/design the prototype and firm up the technical and business plan. If needed, teams may also be supervised by faculty/alumni/industry mentors.</div> 
-                    </div>
+            <div className="para">
+              <div className="phasediv">
+                <div className="phase">PHASE3</div>
+              </div>
+              <div>
+                Selected 50 teams from Phase 2 will participate in Phase 3. It
+                is a 2 months long phase where shortlisted teams will
+                develop/design the prototype and firm up the technical and
+                business plan. If needed, teams may also be supervised by
+                faculty/alumni/industry mentors.
+              </div>
+            </div>
 
-                    <div className="para">
-                        <div className="phasediv">
-                        <div className="phase">PHASE4</div>
-                        </div>
-                    <div>Selected 30 teams will present technical and business plan for their Startup. Teams will be evaluated. There will be provision of upto Rs. 20 Lakhs funding for each team selected for funding.</div> 
-                    </div>
+            <div className="para">
+              <div className="phasediv">
+                <div className="phase">PHASE4</div>
+              </div>
+              <div>
+                Selected 30 teams will present technical and business plan for
+                their Startup. Teams will be evaluated. There will be provision
+                of upto Rs. 20 Lakhs funding for each team selected for funding.
+              </div>
+            </div>
 
-                    <div className="register">
-                        <a target="_blank" href="https://forms.gle/JTDosdQ1dwoCRcgu5" className='registertext'>Register Now</a>
-                    </div>
-                </div>
-                {/* <div className="aboutimages">
+            <div className="register">
+              <a
+                target="_blank"
+                href="https://chat.whatsapp.com/La4TUDAeZESFWKVN0fYzZa"
+                className="registertext"
+              >
+                whatsapp group
+              </a>
+            </div>
+          </div>
+          {/* <div className="aboutimages">
                     <div className="image">
                         <img src={image} alt="image" />
                     </div>
@@ -54,21 +79,21 @@ const About = () => {
                         <img src={image} alt="image" />
                     </div>
                 </div> */}
-                <ul className="circles">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-            </div>
-        </>
-    )
+          <ul className="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </>
+    );
 }
 
 export default About;
--- src/Components/Timeline/Timeline.jsx
+++ src/Components/Timeline/Timeline.jsx
@@ -8,83 +8,112 @@ const Timeline = () => {
         AOS.init({ offset: 140 });
     }, [])
     return (
-        <>
-            <div className="timelinesection grid-bg ba-grid anim">
-                <div className="inner"></div>
-                <div className="timeline-heading">&lt;timeline&gt;</div>
-                <div className="timeline">
-                    <div className="container left-container" data-aos='fade-in'>
-                        {/* <img src={logo} alt="" /> */}
-                        <div className="text-box" data-aos='fade-left'>
-                            <h2>19th April 2024</h2>
-                            <p>From 5PM onwards</p>
-                            <p>opening ceremony (Attandance Mandatory) + Speaker Session</p>
-                            <p>Auditorium</p>
-                            <span className='left-container-arrow'></span>
-                        </div>
-                    </div>
-                    <div className="container right-container">
-                        {/* <img src={logo} alt="" /> */}
-                        <div className="text-box" data-aos='fade-right'>
-                            <h2>19th April 2024</h2>
-                            <p>From 6PM onwards</p>
-                            <p>Beginning of the Hackathon</p>
-                            <p>online mode</p>
-                            <span className='right-container-arrow'></span>
-                        </div>
-                    </div>
-                    <div className="container left-container">
-                        {/* <img src={logo} alt="" /> */}
-                        <div className="text-box" data-aos='fade-left'>
-                            <h2>19th April 2024</h2>
-                            <p>6PM to 9PM</p>
-                            <p>First Round of Evaluation</p>
-                            <p>online mode</p>
-                            <span className='left-container-arrow'></span>
-                        </div>
-                    </div>
-                    <div className="container right-container">
-                        {/* <img src={logo} alt="" /> */}
-                        <div className="text-box" data-aos='fade-right'>
-                            <h2>9PM onwards on 19th April - 1PM on 20th April</h2>
-                            <p>Development of the Projects</p>
-                            <p>online mode</p>
-                            <span className='right-container-arrow'></span>
-                        </div>
-                    </div>
-                    <div className="container left-container">
-                        {/* <img src={logo} alt="" /> */}
-                        <div className="text-box" data-aos='fade-left'>
-                            <h2>20th April 2024</h2>
-                            <p>1PM to 7PM</p>
-                            <p>Development And Mid Evaluation</p>
-                            <p>Auditorium</p>
-                            <span className='left-container-arrow'></span>
-                        </div>
-                    </div>
-                    <div className="container right-container">
-                        {/* <img src={logo} alt="" /> */}
-                        <div className="text-box" data-aos='fade-right'>
-                            <h2>7PM onwards on 20th April - 9AM on 21th April</h2>
-                            <p>Development of the Projects</p>
-                            <p>online mode</p>
-                            <span className='right-container-arrow'></span>
-                        </div>
-                    </div>
-                    <div className="container left-container">
-                        {/* <img src={logo} alt="" /> */}
-                        <div className="text-box" data-aos='fade-left'>
-                            <h2>21th April 2024</h2>
-                            <p>9AM to 6PM</p>
-                            <p>Development And Final Evaluation</p>
-                            <p>Auditorium</p>
-                            <span className='left-container-arrow'></span>
-                        </div>
-                    </div>
-                </div>
+      <>
+        <div className="timelinesection grid-bg ba-grid anim">
+          <div className="inner"></div>
+          <div className="timeline-heading">&lt;timeline&gt;</div>
+          <div className="timeline">
+            <div className="container left-container" data-aos="fade-in">
+              {/* <img src={logo} alt="" /> */}
+              <div className="text-box" data-aos="fade-left">
+                <h2>28th September 2024</h2>
+                <p>10AM onwards</p>
+                <p>Welcome and inaugration ceremony (attendance mandatory)</p>
+                <p>Auditorium</p>
+                <span className="left-container-arrow"></span>
+              </div>
             </div>
-        </>
-    )
+            <div className="container right-container">
+              {/* <img src={logo} alt="" /> */}
+              <div className="text-box" data-aos="fade-right">
+                <h2>28th September 2024</h2>
+                <p>11:30 AM to 1:30 PM</p>
+                <p>First round of evaluation</p>
+                <span className="right-container-arrow"></span>
+              </div>
+            </div>
+            <div className="container left-container">
+              {/* <img src={logo} alt="" /> */}
+              <div className="text-box" data-aos="fade-left">
+                <h2>28th September 2024</h2>
+                <p>1:30 PM to 2:30 PM</p>
+                <p>Lunch</p>
+                <span className="left-container-arrow"></span>
+              </div>
+            </div>
+            <div className="container right-container">
+              {/* <img src={logo} alt="" /> */}
+              <div className="text-box" data-aos="fade-right">
+                <h2>28th September 2024</h2>
+                <p>2:30 PM to 5:00 PM</p>
+                <p>Discussion sessions for further improvement</p>
+                <p>Auditorium</p>
+                <span className="right-container-arrow"></span>
+              </div>
+            </div>
+            <div className="container left-container">
+              {/* <img src={logo} alt="" /> */}
+              <div className="text-box" data-aos="fade-left">
+                <h2>28th September 2024</h2>
+                <p>5:15 PM to 6:15 PM</p>
+                <p>Tech talk from the experts</p>
+                <p>Auditorium</p>
+                <span className="left-container-arrow"></span>
+              </div>
+            </div>
+            <div className="container right-container">
+              {/* <img src={logo} alt="" /> */}
+              <div className="text-box" data-aos="fade-right">
+                <h2>29th September 2024</h2>
+                <p>9:30 AM</p>
+                <p>Gathering of all participating teams</p>
+                <p>Auditorium</p>
+                <span className="right-container-arrow"></span>
+              </div>
+            </div>
+            <div className="container left-container">
+              {/* <img src={logo} alt="" /> */}
+              <div className="text-box" data-aos="fade-left">
+                <h2>29th September 2024</h2>
+                <p>10 AM to 11 AM</p>
+                <p>Tech talk from the experts</p>
+                <p>Auditorium</p>
+                <span className="left-container-arrow"></span>
+              </div>
+            </div>
+            <div className="container right-container">
+              {/* <img src={logo} alt="" /> */}
+              <div className="text-box" data-aos="fade-right">
+                <h2>29th September 2024</h2>
+                <p>11:15 AM to 1:30 PM</p>
+                <p>Startup Workshop</p>
+                <p>Auditorium</p>
+                <span className="right-container-arrow"></span>
+              </div>
+            </div>
+            <div className="container left-container">
+              {/* <img src={logo} alt="" /> */}
+              <div className="text-box" data-aos="fade-left">
+                <h2>29th September 2024</h2>
+                <p>1:30 PM to 2:00 PM</p>
+                <p>Closing ceremony</p>
+                <p>Auditorium</p>
+                <span className="left-container-arrow"></span>
+              </div>
+            </div>
+            <div className="container right-container">
+              {/* <img src={logo} alt="" /> */}
+              <div className="text-box" data-aos="fade-right">
+                <h2>29th September 2024</h2>
+                <p>2:30 PM to 3:00 PM</p>
+                <p>Lunch</p>
+                <span className="right-container-arrow"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
 }
 
 export default Timeline;
